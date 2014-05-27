472b7ad12e65db7c1733932af6c9b47eb142ef11
ILYONG CHO <ilyoan@gmail.com>
2014-05-28 01:34:41 +0900

    working on resv thread

--- src/com/ilyoan/mytrainresv/core/MyTrainResv.java
+++ src/com/ilyoan/mytrainresv/core/MyTrainResv.java
@@ -1,6 +1,9 @@
 package com.ilyoan.mytrainresv.core;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import android.app.Activity;
 import android.util.Log;
@@ -103,31 +106,108 @@ public class MyTrainResv {
 
 		Log.d(TAG, "MyTrainResv.resvTrain()");
 
-		// Register train list handler.
-		this.http.setOnSearchTrainCallback(this.onSearchTrainForResvCallback);
-		// Search trains.
-		this.http.searchTrain(
-				this.lastSearchOption.stationFrom,
-				this.lastSearchOption.stationTo,
-				this.lastSearchOption.date,
-				this.lastSearchOption.timeFrom,
-				this.lastSearchOption.ktxOnly);
+		// Stop the thread
+		this.trainResvThread.stopThread();
+		// Start with new parameter
+		this.trainResvThread.startThread(trainList, this.lastSearchOption);
+
+
 	}
 
-	// Train list are obtained, see if there is a train available.
-	private final MyHttp.OnSearchTrainCallback onSearchTrainForResvCallback = new MyHttp.OnSearchTrainCallback() {
-		@Override
-		public void onResult(ArrayList<Train> trainList, String error) {
-			if (error != null) {
-				// If there was an error show toast message.
-				showToast(error);
-			} else {
-				// Do reservation work.
-				showToast("do resv");
+	// Train reservation thread.
+	TrainResvThread trainResvThread = new TrainResvThread();
+	class TrainResvThread extends Thread {
+		// The list of train that is wanted to be reserved.
+		private HashSet<String> wishTrainNo = null;
+		// train search option
+		private TrainSearchOption searchOption = null;
+		//
+		private boolean running = false;
+		private final Lock searchLoopLock = new ReentrantLock(true);
+
+
+		public void startThread(
+				final ArrayList<Train> wishList,
+				final TrainSearchOption searchOption) {
+			// Set parameters to search and reserve train.
+			this.wishTrainNo = new HashSet<String>();
+			for (Train train : wishList) {
+				this.wishTrainNo.add(train.no);
+			}
+			this.searchOption = searchOption;
+
+			// Set this thread is running.
+			this.running = true;
+
+			// Start thread.
+			start();
+		}
+
+		public void stopThread() {
+			// To halt thread loop.
+			this.running = false;
+			// If the thread is running than wait until it dies.
+			if (isAlive()) {
+				try {
+					join();
+				} catch (InterruptedException e) {
+					showToast(e.getMessage());
+				}
 			}
 		}
 
+		@Override
+		public void run() {
+			while (this.running) {
+				this.searchLoopLock.lock();
+				Log.d(TAG, "Resv thread running");
+
+				// Register train list handler.
+				MyTrainResv.this.http.setOnSearchTrainCallback(
+						this.onSearchTrainForResvCallback);
+				// Search trains.
+				MyTrainResv.this.http.searchTrain(
+						this.searchOption.stationFrom,
+						this.searchOption.stationTo,
+						this.searchOption.date,
+						this.searchOption.timeFrom,
+						this.searchOption.ktxOnly);
+			}
+		}
+
+		// Train list are obtained, see if there is a train available.
+		private final MyHttp.OnSearchTrainCallback onSearchTrainForResvCallback = new MyHttp.OnSearchTrainCallback() {
+			@Override
+			public void onResult(ArrayList<Train> trainList, String error) {
+				if (error != null) {
+					// If there was an error show toast message.
+					showToast(error);
+				} else {
+					// Iterate over the train list.
+					for (Train train: trainList) {
+						// We got a train available.
+						if (train.hasNormal || train.hasSpecial && MyTrainResv.this.lastSearchOption.firstClass) {
+							// And the train is one of wanted.
+							if (TrainResvThread.this.wishTrainNo.contains(train.no)) {
+								// Try reservation
+								TryResvTrain(train);
+							}
+						}
+					}
+				}
+				Log.d(TAG, "Resv thread notify");
+				TrainResvThread.this.searchLoopLock.unlock();
+			}
+		};
+
+		public void TryResvTrain(Train train) {
+			Log.d(TAG, "MyTrainResv.TryResvTrain()");
+			Log.i(TAG, "Try resv train - " + train.toString());
+
+			//MyTrainResv.this.http.resv(train);
+		}
 	};
+
 
 	// Show toast message.
 	public static void showToast(String message) {
@@ -148,7 +228,7 @@ public class MyTrainResv {
 		this.noti = noti;
 	}
 
-	// Train searcing option.
+	// Train searching option.
 	public class TrainSearchOption {
 		public TrainSearchOption(
 				String stationFrom,
