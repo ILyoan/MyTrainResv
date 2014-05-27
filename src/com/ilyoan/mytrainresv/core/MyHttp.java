7ddf53438e6019fd0ef10ee99f3ff5ec6b760164
ILYONG CHO <ilyoan@gmail.com>
2014-05-28 00:44:20 +0900

    Add comments

--- src/com/ilyoan/mytrainresv/core/MyHttp.java
+++ src/com/ilyoan/mytrainresv/core/MyHttp.java
@@ -47,14 +47,17 @@ public class MyHttp {
 
 	private OnSearchTrainCallback onSearchTrainCallback = null;
 
+	// Interface for search train result handler.
 	public interface OnSearchTrainCallback {
 		public void onResult(ArrayList<Train> trainList, String error);
 	}
 
+	// Register search train result handler.
 	public void setOnSearchTrainCallback(OnSearchTrainCallback cb) {
 		this.onSearchTrainCallback = cb;
 	}
 
+	// login to system.
 	public void login(String id, String pw) {
 		Log.d(TAG, "MyHttp.login(" + id + ")");
 
@@ -78,6 +81,7 @@ public class MyHttp {
 		}
 	}
 
+	// login response handler.
 	private final OnResponse onLoginResponse = new OnResponse() {
 		@Override
 		public void onResponse(int status, String content) {
@@ -93,6 +97,7 @@ public class MyHttp {
 		}
 	};
 
+	// search train
 	public void searchTrain(String stationFrom,
 			String stationTo,
 			String date,
@@ -129,6 +134,7 @@ public class MyHttp {
 		}
 	}
 
+	// search result handler.
 	private final OnResponse onSearchTrainResponse = new OnResponse() {
 		@Override
 		public void onResponse(int status, String content) {
@@ -183,13 +189,14 @@ public class MyHttp {
 				Log.v(TAG, train.toString());
 				result.add(train);
 			}
+			// Calls back handler.
 			if (MyHttp.this.onSearchTrainCallback != null) {
 				MyHttp.this.onSearchTrainCallback.onResult(result, null);
 			}
 		}
 	};
 
-
+	// reserve train
 	public void resv(Train train) {
 		Log.d(TAG, "MyHttp.resv(" + train.toString() + ")");
 
@@ -226,6 +233,7 @@ public class MyHttp {
 		}
 	}
 
+	// reserve result handler.
 	private final OnResponse onResvResponse = new OnResponse() {
 		@Override
 		public void onResponse(int status, String content) {
@@ -252,10 +260,10 @@ public class MyHttp {
 		}
 	};
 
-
-
+	// Get list of content string in `text` that reside between `begin` and `end` finger print.
 	private final ArrayList<Pair<String, Integer>> getStringBetweenFingerprint(
 			String text, String begin, String end) {
+		// Result list.
 		ArrayList<Pair<String, Integer>> res = new ArrayList<Pair<String, Integer>>();
 		int beginIndex = 0;
 		while (beginIndex != -1) {
@@ -270,6 +278,7 @@ public class MyHttp {
 		return res;
 	}
 
+	// Get content string in `text` that is either `a` or `b` beginning from `beginIndex`.
 	private final Pair<String, Integer> getFirstMatched(String text, String a, String b, int beginIndex) {
 		int indexA = text.indexOf(a, beginIndex);
 		int indexB = text.indexOf(b, beginIndex);
@@ -287,10 +296,12 @@ public class MyHttp {
 		}
 	}
 
+	// Get status code of http response
 	private final int getStatusCode(HttpResponse response) {
 		return response.getStatusLine().getStatusCode();
 	}
 
+	// Get content string.
 	private final String getContentString(HttpEntity entity) {
 		return getContentString(entity, "UTF-8");
 	}
@@ -327,12 +338,15 @@ public class MyHttp {
 		return sb.toString();
 	}
 
+	// Interface for http response handler.
 	public interface OnResponse {
 		public void onResponse(int status, String content);
 	}
 
+	// Http request method.
 	public enum Method { GET, POST }
 
+	// New version of Android enforces to use HttpTask.
 	private class HttpTask extends AsyncTask<String, Void, HttpResponse> {
 		private final Method method;
 		private final String url;
@@ -341,8 +355,10 @@ public class MyHttp {
 		private int status;
 		private String contents;
 
+		// HttpClient object. the object will be created in the parent class(MyHttp) for persistent connection.
 		private HttpClient httpClient = null;
 
+		// Create new HttpTask object.
 		public HttpTask(HttpClient httpClient, Method method, String url, OnResponse onResponse) {
 			this.httpClient = httpClient;
 			this.method = method;
@@ -350,6 +366,9 @@ public class MyHttp {
 			this.onResponse = onResponse;
 		}
 
+		// Set post entity
+		// TODO(ilyoan): looks like this is not working from some point.
+		// take look at this. fortunately using GET method works.
 		public void setPostEntity(List<NameValuePair> entities) {
 			try {
 				this.postEntity = new UrlEncodedFormEntity(entities);
@@ -359,11 +378,13 @@ public class MyHttp {
 			}
 		}
 
+		// Http request procedure running in background.
 		@Override
 		protected HttpResponse doInBackground(String... params) {
 			HttpUriRequest request = null;
 			HttpResponse response = null;
 
+			// Create http request object depends on the method.
 			if (this.method == Method.GET) {
 				request = new HttpGet(this.url);
 			} else {
@@ -373,11 +394,13 @@ public class MyHttp {
 				}
 				request = post;
 			}
+			// Set request header.
 			request.addHeader("Content-Type", "application/xhtml+xml");
 			request.addHeader("charset", "UTF-8");
 			request.addHeader("Referer", URL_RESV_REFERER);
 
 			try {
+				// Take http response
 				response = this.httpClient.execute(request);
 				this.status = getStatusCode(response);
 				this.contents = getContentString(response.getEntity());
@@ -388,9 +411,11 @@ public class MyHttp {
 				Log.e(TAG, "HttpTask failed: " + e.getMessage());
 				e.printStackTrace();
 			}
+			// Returns the http response. onPostExecute() will be invoked by Android system.
 			return response;
 		}
 
+		// Now we got Http response object. process with it. this function runs on main thread.
 		@Override
 		protected void onPostExecute(HttpResponse response) {
 			this.onResponse.onResponse(this.status, this.contents);
--- src/com/ilyoan/mytrainresv/core/MyTrainResv.java
+++ src/com/ilyoan/mytrainresv/core/MyTrainResv.java
@@ -9,17 +9,25 @@ import android.widget.Toast;
 public class MyTrainResv {
 	private static final String TAG = "MyTrainResv";
 
+	// Singleton object.
 	private static MyTrainResv instance = null;
+
+	// Activity.
 	private static Activity activity = null;
 
+	// MyHttp object.
 	private final MyHttp http = new MyHttp();
 
+	// Notification handler.
 	private OnNotification noti = null;
 
+	// Last used searching option for train list.
 	private TrainSearchOption lastSearchOption = null;
 
+	// Prevent creating an object of this class as this is singleton.
 	private MyTrainResv() { }
 
+	// Get the singleton object.
 	public static MyTrainResv getInstance() {
 		if (instance == null) {
 			instance = new MyTrainResv();
@@ -27,15 +35,18 @@ public class MyTrainResv {
 		return instance;
 	}
 
+	// Set activity object.
 	public static void setActivity(Activity activity) {
 		MyTrainResv.activity = activity;
 	}
 
+	// Login to server.
 	public void login(String id, String pw) {
 		Log.d(TAG, "MyTrainResv.login(" + id + ")");
 		this.http.login(id, pw);
 	}
 
+	// Search train list.
 	public void searchTrain(String stationFrom,
 			String stationTo,
 			String date,
@@ -45,6 +56,8 @@ public class MyTrainResv {
 			boolean ktxOnly) {
 		Log.d(TAG, "MyTrainResv.searchTrain()");
 
+		// Update last used search option.
+		// This option will be used on resvTrain() function.
 		Station station = Station.getInstance();
 		this.lastSearchOption = new TrainSearchOption(
 				station.getCode(stationFrom),
@@ -55,7 +68,9 @@ public class MyTrainResv {
 				firstClass,
 				ktxOnly);
 
+		// Set handler.
 		this.http.setOnSearchTrainCallback(this.onSearchTrainCallback);
+		// Do search.
 		this.http.searchTrain(
 				station.getCode(stationFrom),
 				station.getCode(stationTo),
@@ -64,26 +79,33 @@ public class MyTrainResv {
 				ktxOnly);
 	}
 
+	// Train list result handler.
 	private final MyHttp.OnSearchTrainCallback onSearchTrainCallback = new MyHttp.OnSearchTrainCallback() {
 		@Override
 		public void onResult(ArrayList<Train> trainList, String error) {
 			if (error != null) {
+				// If there was an error, show toast message.
 				showToast(error);
 			} else if (MyTrainResv.this.noti != null) {
+				// Give notification via noti handler.
 				MyTrainResv.this.noti.onTrainList(trainList);
 			}
 		}
 
 	};
 
+	// Reserve train
 	public void resvTrain(ArrayList<Train> trainList) {
+		// Trains should have been search at least once.
 		if (this.lastSearchOption == null) {
 			return;
 		}
 
 		Log.d(TAG, "MyTrainResv.resvTrain()");
 
+		// Register train list handler.
 		this.http.setOnSearchTrainCallback(this.onSearchTrainForResvCallback);
+		// Search trains.
 		this.http.searchTrain(
 				this.lastSearchOption.stationFrom,
 				this.lastSearchOption.stationTo,
@@ -92,18 +114,22 @@ public class MyTrainResv {
 				this.lastSearchOption.ktxOnly);
 	}
 
+	// Train list are obtained, see if there is a train available.
 	private final MyHttp.OnSearchTrainCallback onSearchTrainForResvCallback = new MyHttp.OnSearchTrainCallback() {
 		@Override
 		public void onResult(ArrayList<Train> trainList, String error) {
 			if (error != null) {
+				// If there was an error show toast message.
 				showToast(error);
 			} else {
+				// Do reservation work.
 				showToast("do resv");
 			}
 		}
 
 	};
 
+	// Show toast message.
 	public static void showToast(String message) {
 		Toast toast = Toast.makeText(
 				MyTrainResv.activity.getApplicationContext(),
@@ -112,14 +138,17 @@ public class MyTrainResv {
 		toast.show();
 	}
 
+	// Interface for notifying to the main activity or wrapper system.
 	public interface OnNotification {
 		public void onTrainList(ArrayList<Train> trainList);
 	}
 
+	// Register notification.
 	public void setNotification(OnNotification noti) {
 		this.noti = noti;
 	}
 
+	// Train searcing option.
 	public class TrainSearchOption {
 		public TrainSearchOption(
 				String stationFrom,
